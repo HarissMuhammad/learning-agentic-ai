5f30858442ea409bdfbdda32fef966ab73849edc
HarissMuhammad <haris928762@gmail.com>
2025-09-08 16:00:04 +0500

    changed inputs

--- agent_sdk/projects/deep_research/main.py
+++ agent_sdk/projects/deep_research/main.py
@@ -1,11 +1,23 @@
 import asyncio
 from dotenv import load_dotenv
 from rich.console import Console
+from rich.prompt import Prompt
+
+load_dotenv()
+console = Console()
 
 async def main():
-    print("env initiated")
-    load_dotenv()
-    console = Console()
-
+    console.print("[bold blue] Deep Research Agent [/bold blue] - Console Edition")
+    console.print("This Tool performs in-depth research on any topic using AI agents.")
+    
+    #get the users Query
+    
+    query = Prompt.ask("[bold]What would you like to reasearch[/bold]")
+    
+    if not query.strip():
+        console.print("[red]Error:[/red] Query cannot be empty.")
+        return
+    
+    
 if __name__ == "__main__":
     asyncio.run(main())
