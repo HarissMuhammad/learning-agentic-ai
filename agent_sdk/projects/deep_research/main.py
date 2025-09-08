29b2f1d5d363f77f5709ffdcacad7a6393ef4947
HarissMuhammad <haris928762@gmail.com>
2025-09-08 15:44:18 +0500

    added base

--- agent_sdk/projects/deep_research/main.py
+++ agent_sdk/projects/deep_research/main.py
@@ -1,6 +1,11 @@
-def main():
-    print("Hello from deep-research!")
+import asyncio
+from dotenv import load_dotenv
+from rich.console import Console
 
+async def main():
+    print("env initiated")
+    load_dotenv()
+    console = Console()
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
